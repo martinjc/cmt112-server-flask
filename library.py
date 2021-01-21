aae812df8e7cb180e3b57bc1c1a398fff8940602
Martin Chorley <chorleymj@cardiff.ac.uk>
2021-01-21 13:57:17 +0000

    add CORS

--- library.py
+++ library.py
@@ -1,14 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///library.sqlite"
 db = SQLAlchemy(app)
-
-
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
 
 
 import routes.authors
